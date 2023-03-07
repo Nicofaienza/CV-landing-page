99d5db3b791ce2610683f261c362460651b203d4
Nicolas Faienza <nicolas.faienza1@gmail.com>
2023-03-07 13:55:06 -0300

    update hero info

--- src/layouts/Hero/Hero.tsx
+++ src/layouts/Hero/Hero.tsx
@@ -47,8 +47,9 @@ export default function Hero(): JSX.Element {
           <HeroTitle>👋 Hola, soy Desarrollador Frontend.</HeroTitle>
           <HeroText>
             Estudié de manera autodidacta el desarrollo de aplicaciones del lado
-            del frontend con <strong>HTML5</strong>, <strong>CSS3</strong>,{" "}
-            <strong>JavaScript vanilla</strong> y <strong>React JS</strong>.
+            del frontend con <strong>HTML5</strong>, <strong>CSS3</strong>,
+            <strong>JavaScript</strong>, <strong>TypeScript</strong> y{" "}
+            <strong>React JS</strong>.
             <br />
             <br />
             Estoy en búsqueda de mi primer trabajo como desarrollador con el fin
